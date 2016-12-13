3d42f5e7699916f71508d85febead0285919d3fd
Marco Arnoldi <Marco Arnoldi>
2016-12-13 17:58:19 +0100

    Implementazione pagina home

---
+++ index.js
@@ -0,0 +1,39 @@
+
+//express lib
+var express = require('express');
+//inspect
+var util = require('util');
+
+//instantiate express
+var app = express();
+
+//POST
+var bodyParser = require('body-parser');
+
+// PER INCLUDERE I FILE CSS E IL FILE SCRIPT.JS
+app.use(express.static(__dirname+'/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//IMPOSTATA LA PORTA
+app.set('port', (process.env.PORT || 1338));
+
+
+/**
+ * @brief RESTITUISCE LA PAGINA HOME
+ */
+app.get('/home',function(req,res)
+{
+    res.sendFile(__dirname + '/home.html'); 
+});
+
+
+
+//INFORMARE L'UTENTE DELLA PORTA CHE SI UTILIZZA E DELL'URL DELLA PAGINA DI LOGIN
+
+app.listen(app.get('port'), function()           
+{
+    console.log('Node app is running on port', app.get('port'));
+    console.log('Server running at http://127.0.0.1:1338/home');   
+});
+
